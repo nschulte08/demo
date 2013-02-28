e85f5c5272c24bce18698aee010cc17fd84bcf1a
Nick Schulte <nick.schulte@parivedasolutions.com>
2013-02-28 16:03:27 -0600

    Initial project move

---
+++ NicksDemoProject/NicksDemoProject/Controllers/HTMLController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NicksDemoProject.Controllers
+{
+    public class HTMLController : Controller
+    {
+        //
+        // GET: /HTML/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NicksDemoProject/NicksDemoProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NicksDemoProject.MvcApplication" Language="C#" %>
